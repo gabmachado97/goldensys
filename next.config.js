a222e9ffa22935036ecc7c8914e7f2c6d7e8fe81
Gabriel Machado <61938035+gabmachado97@users.noreply.github.com>
2024-05-28 12:12:13 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-  };
-  
-  if (process.env.NODE_ENV === 'production') {
-    // Use "export" output target for production builds
-    nextConfig.exportPathMap = function() {
-      return {
-        '/': { page: '/' },
-        // Add other pages here if needed
-      };
-    };
-  } else {
-    // Use default output target for development
-    // No need to specify anything here
+    output: 'export',
   }
   
   module.exports = nextConfig;
